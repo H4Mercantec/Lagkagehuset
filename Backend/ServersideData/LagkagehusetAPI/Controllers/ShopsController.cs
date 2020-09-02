164ddce3406eef07dfb5fb3ec826e91d15d5b689
Maria Clemmensen <clemmensen.maria@gmail.com>
2020-09-02 11:16:44 +0200

    Add /shops/{city} temp test data

--- Backend/ServersideData/LagkagehusetAPI/Controllers/ShopsController.cs
+++ Backend/ServersideData/LagkagehusetAPI/Controllers/ShopsController.cs
@@ -15,7 +15,65 @@ namespace LagkagehusetAPI.Controllers
     {
         private readonly ILogger<ShopsController> _logger;
 
-        public ShopsController(ILogger<ShopsController> logger)
+        private List<ShopModel> tempTestShops = new List<ShopModel>() {
+            new ShopModel()
+            {
+                shopID = 1,
+                address = "Søndergade 2",
+                city = "Viborg",
+                phoneNumber = "80808080",
+                postCode = "8800",
+                Inventories = new List<InventoryModel>()
+                {
+                    new InventoryModel()
+                    {
+                        inventoryID = 1,
+                        amount = 154,
+                        product = new ProductModel()
+                        {
+                            productID = 1,
+                            name = "Giffel"
+                        }
+                    },
+
+                    new InventoryModel()
+                    {
+                        inventoryID = 2,
+                        amount = 3,
+                        product = new ProductModel()
+                        {
+                            productID = 2,
+                            name = "Københavner birkes"
+                        }
+                    }
+                }
+            },
+            new ShopModel()
+            {
+                shopID = 2,
+                address = "Vestergade 26",
+                city = "Birkerød",
+                phoneNumber = "54235868",
+                postCode = "3460",
+                Inventories = new List<InventoryModel>()
+                {
+                    new InventoryModel()
+                    {
+                        inventoryID = 1,
+                        amount = 10,
+                        product = new ProductModel()
+                        {
+                            productID = 1,
+                            name = "Giffel"
+                        }
+                    }
+                }
+            }
+        };
+
+
+
+    public ShopsController(ILogger<ShopsController> logger)
         {
             _logger = logger;
         }
@@ -23,64 +81,26 @@ namespace LagkagehusetAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<List<ShopModel>>> Get(bool includeRollData = false)
         {
-
-            var tempTestShops = new List<ShopModel>() {
-                new ShopModel()
+            var tempData = tempTestShops;
+            if (!includeRollData)
+            {
+                foreach (var shop in tempTestShops)
                 {
-                    shopID = 1,
-                    address = "Søndergade 2",
-                    city = "Viborg",
-                    phoneNumber = "80808080",
-                    postCode = "8800",
-                    Inventories = !includeRollData? null : new List<InventoryModel>()
-                    {
-                        new InventoryModel()
-                        {
-                            inventoryID = 1,
-                            amount = 154,
-                            product = new ProductModel()
-                            {
-                                productID = 1,
-                                name = "Giffel"
-                            }
-                        },
-
-                        new InventoryModel()
-                        {
-                            inventoryID = 2,
-                            amount = 3,
-                            product = new ProductModel()
-                            {
-                                productID = 2,
-                                name = "Københavner birkes"
-                            }
-                        }
-                    }
-                },
-                new ShopModel()
-                {
-                    shopID = 2,
-                    address = "Vestergade 26",
-                    city = "Birkerød",
-                    phoneNumber = "54235868",
-                    postCode = "3460",
-                    Inventories = !includeRollData? null : new List<InventoryModel>()
-                    {
-                        new InventoryModel()
-                        {
-                            inventoryID = 1,
-                            amount = 10,
-                            product = new ProductModel()
-                            {
-                                productID = 1,
-                                name = "Giffel"
-                            }
-                        }
-                    }
+                    shop.Inventories = null;
                 }
-            };
-
+            }
             return tempTestShops;
+        }
+
+
+
+        [HttpGet("{city}")]
+        public async Task<ActionResult<ShopModel>> Get(string city)
+        {
+            var shopSelected = tempTestShops.FirstOrDefault(shop => shop.city == city);
+            if (shopSelected == null) return NotFound($"{city} was not found");
+
+            return shopSelected;
         }
     }
 }
