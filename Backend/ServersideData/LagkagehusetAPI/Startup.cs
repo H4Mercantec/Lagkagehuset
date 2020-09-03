77fa4dd30c5364674e669d88ef7b029f73334e81
Maria Clemmensen <clemmensen.maria@gmail.com>
2020-09-03 11:48:47 +0200

    Allow static files delivery from the API for now to facilitate testing

--- Backend/ServersideData/LagkagehusetAPI/Startup.cs
+++ Backend/ServersideData/LagkagehusetAPI/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -45,6 +48,15 @@ namespace LagkagehusetAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // For testing purposes ;)
+            var pathToFrontend = Path.Join("..", "..", "..", "Frontend");
+            var pathToStaticFiles = Path.Join(Directory.GetCurrentDirectory(), pathToFrontend);
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(pathToStaticFiles),
+                RequestPath = new PathString("")
             });
         }
     }
