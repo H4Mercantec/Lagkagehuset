a92660295116f39c5776be219023d603f7f96265
Alexander <ak@kk-partner.dk>
2020-09-04 13:27:49 +0200

    Auto stash before merge of "master" and "origin/master"

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -20,11 +20,32 @@ function switchStore(elm) {
 }
 
 function resetBasket() {
-    for (var i = 0; i < rundstykkerImg.length; i++) {
-        var optElm = document.getElementById(rundstykkerImg[i]);
+    for (var i = 0; i < rolls.length; i++) {
+        var optElm = document.getElementById(rolls[i]);
         if (optElm == undefined) continue;
         optElm.value = 0;
     }
+} 
+
+function loadApi()
+{
+    //url = "http://jsonplaceholder.typicode.com/users"; 
+    url = "/shops?includeRollData=true";// todoo tilføj url til api
+  var api = new XMLHttpRequest();
+  api.open("GET", url, true);
+      
+  api.onerror = () => { addAllShops(); }
+  api.onabort = () => { addAllShops(); }
+  api.ontimeout = () => { addAllShops(); }
+  api.onload = () => {
+      try {
+          apiArray = JSON.parse(api.response);
+          window.apiArray = apiArray;
+          addAllShops();
+      } catch
+      {addAllShops();}
+  }
+  api.send();
 }
 
 
